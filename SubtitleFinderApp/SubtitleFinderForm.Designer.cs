3fb3258f0bdb3fcd69dc0abf9dce3f3d9995d986
Gustavo Vasquez <vasquez.mgustavo@gmail.com>
2019-03-08 20:04:56 -0300

    Se puede obtener la información necesaria y descargar el subtitulo. Todo muy preliminar

--- SubtitleFinderApp/SubtitleFinderForm.Designer.cs
+++ SubtitleFinderApp/SubtitleFinderForm.Designer.cs
@@ -31,25 +31,34 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.lblComments = new System.Windows.Forms.LinkLabel();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 214);
+            this.textBox1.Location = new System.Drawing.Point(12, 306);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(834, 225);
+            this.textBox1.Size = new System.Drawing.Size(589, 133);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(771, 37);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(607, 285);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -59,33 +68,37 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 88);
+            this.textBox2.Location = new System.Drawing.Point(12, 211);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(834, 120);
+            this.textBox2.Size = new System.Drawing.Size(589, 76);
             this.textBox2.TabIndex = 3;
             // 
-            // textBox3
+            // txtSearch
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 40);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(377, 20);
-            this.textBox3.TabIndex = 4;
+            this.txtSearch.Location = new System.Drawing.Point(12, 40);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(377, 20);
+            this.txtSearch.TabIndex = 4;
+            this.txtSearch.Text = "the walking dead";
             // 
-            // button2
+            // btnSearch
             // 
-            this.button2.Location = new System.Drawing.Point(395, 38);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Buscar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSearch.Location = new System.Drawing.Point(395, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(555, 39);
             this.comboBox1.Name = "comboBox1";
@@ -110,16 +123,96 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Escriba el nombre de la serie/película:";
             // 
+            // btnDownload
+            // 
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDownload.Location = new System.Drawing.Point(571, 93);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(105, 55);
+            this.btnDownload.TabIndex = 9;
+            this.btnDownload.Text = "Descargar";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.lblTitle, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDescription, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblDetails, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 93);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.79105F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.20895F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(552, 95);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoEllipsis = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(3, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(553, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "TITULO";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoEllipsis = true;
+            this.lblDescription.Location = new System.Drawing.Point(3, 29);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(549, 40);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "DESCRIPCION";
+            this.lblDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoEllipsis = true;
+            this.lblDetails.Location = new System.Drawing.Point(3, 69);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(553, 26);
+            this.lblDetails.TabIndex = 2;
+            this.lblDetails.Text = "DETALLES";
+            this.lblDetails.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblComments
+            // 
+            this.lblComments.Location = new System.Drawing.Point(571, 162);
+            this.lblComments.Name = "lblComments";
+            this.lblComments.Size = new System.Drawing.Size(105, 26);
+            this.lblComments.TabIndex = 11;
+            this.lblComments.TabStop = true;
+            this.lblComments.Text = "Comentarios (30)";
+            this.lblComments.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblComments.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblComments_LinkClicked);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "rar";
+            this.saveFileDialog1.Filter = "Archivos RAR (*.rar)|*.rar|Todos los archivos (*.*)|*.*";
+            this.saveFileDialog1.InitialDirectory = "F:\\Carpeta personal";
+            this.saveFileDialog1.RestoreDirectory = true;
+            this.saveFileDialog1.Title = "Guardar archivo como...";
+            // 
             // SubtitleFinderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(858, 451);
+            this.ClientSize = new System.Drawing.Size(693, 451);
+            this.Controls.Add(this.lblComments);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnDownload);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -127,6 +220,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Buscador de subtítulos";
             this.Load += new System.EventHandler(this.SubtitleFinderForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,11 +231,18 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.LinkLabel lblComments;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- SubtitleFinderApp/SubtitleFinderForm.cs
+++ SubtitleFinderApp/SubtitleFinderForm.cs
@@ -6,8 +6,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace SubtitleFinderApp
@@ -40,6 +42,64 @@ namespace SubtitleFinderApp
             foreach(var d in descriptions)
             {
                 textBox1.Text += d.InnerText;
+            }
+        }
+
+        private void DoSearch(string text)
+        {
+            var htmldoc = _web.Load("https://www.subdivx.com/index.php?buscar=" + HttpUtility.UrlEncode(text) + "&accion=5&masdesc=&subtitulos=1&realiza_b=1");
+            var title = htmldoc.DocumentNode.Descendants("a").Where(a => a.Attributes.Contains("class") && a.Attributes["class"].Value.Contains("titulo_menu_izq")).FirstOrDefault();
+            var description = htmldoc.DocumentNode.Descendants("div").Where(a => a.Attributes.Contains("id") && a.Attributes["id"].Value.Equals("buscador_detalle_sub")).FirstOrDefault();
+            var detail = htmldoc.DocumentNode.Descendants("div").Where(a => a.Attributes.Contains("id") && a.Attributes["id"].Value.Equals("buscador_detalle_sub_datos")).FirstOrDefault();
+            
+            lblTitle.Text = title.InnerText;
+            lblDescription.Text = description.InnerText;
+            lblDetails.Text = detail.InnerText;
+            lblComments.Text = "Comentarios (" + detail.Descendants("a").Where(a => a.Attributes.Contains("rel") && a.Attributes["rel"].Value.Equals("nofollow")).FirstOrDefault().InnerText + ")";
+            lblComments.Links.Add(new LinkLabel.Link() { LinkData = detail.Descendants("a").Where(a => a.Attributes.Contains("href")).FirstOrDefault().Attributes["href"].Value });
+        }
+
+        private void DoSearchAll(string text)
+        {
+            var htmldoc = _web.Load("https://www.subdivx.com/index.php?buscar=" + HttpUtility.UrlEncode(text) + "&accion=5&masdesc=&subtitulos=1&realiza_b=1");
+            var anchors = htmldoc.DocumentNode.Descendants("a").Where(a => a.Attributes.Contains("class") && a.Attributes["class"].Value.Contains("titulo_menu_izq"));
+            var descriptions = htmldoc.DocumentNode.Descendants("div").Where(a => a.Attributes.Contains("id") && a.Attributes["id"].Value.Equals("buscador_detalle_sub"));
+            var details = htmldoc.DocumentNode.Descendants("div").Where(a => a.Attributes.Contains("id") && a.Attributes["id"].Value.Equals("buscador_detalle_sub_datos"));
+
+            foreach (var a in anchors)
+            {
+                lblTitle.Text += a.InnerText;
+            }
+
+            foreach (var d in descriptions)
+            {
+                lblDescription.Text += d.InnerText;
+            }
+
+            foreach (var det in details)
+            {
+                lblDetails.Text += det.InnerText;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.DoSearch(txtSearch.Text);
+        }
+
+        private void lblComments_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.subdivx.com/" + (e.Link.LinkData as string));
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = lblTitle.Text;            
+            var result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {                
+                var wClient = new WebClient();
+                wClient.DownloadFile("http://www.subdivx.com/bajar.php?id=523910&u=8", saveFileDialog1.FileName);
             }
         }
     }
